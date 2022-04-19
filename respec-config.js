c58998da924c06f0dc161624b83202ee21517583
Michael Cooper <cooper@w3.org>
2022-04-19 13:06:08 -0400

    separate respec-config

---
+++ respec-config.js
@@ -0,0 +1,66 @@
+var respecConfig = {
+    // specification status (e.g., WD, LCWD, NOTE, etc.). If in doubt use ED.
+    specStatus:           "ED",
+    //processVersion:       "2014",
+
+    // the specification's short name, as in https://www.w3.org/TR/short-name/
+    shortName:            "fast",
+
+    // if your specification has a subtitle that goes below the main
+    // formal title, define it here
+    // subtitle   :  "an excellent document",
+
+    // if you wish the publication date to be other than today, set this
+    // publishDate:  "2011-12-15",
+
+    // if the specification's copyright date is a range of years, specify
+    // the start date here:
+    // copyrightStart: "2011",
+    license: "document",
+
+    // if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
+    // and its maturity status
+    //previousPublishDate:  "2014-08-14",
+    //previousMaturity:  "WD",
+    //previousDiffURI:        "https://www.w3.org/TR/2014/WD-media-accessibility-reqs-20140814/",
+
+    // if there a publicly available Editor's Draft, this is the link
+    edDraftURI: "https://w3c.github.io/fast/",
+
+    // if this is a LCWD, uncomment and set the end of its review period
+    // lcEnd: "2009-08-05",
+
+    // editors, add as many as you like
+    // only "name" is required
+    editors:  [
+        { name: "Michael Cooper",
+          company: "W3C", companyURL: "https://www.w3.org/" },
+    ],
+
+    // authors, add as many as you like.
+    // This is optional, uncomment if you have authors as well as editors.
+    // only "name" is required. Same format as editors.
+
+    // name of the WG
+    wg:           "Accessible Platform Architectures Working Group",
+
+    // URI of the public WG page
+    wgURI:        "https://www.w3.org/WAI/APA/",
+
+    // name (with the @w3c.org) of the public mailing to which comments are due
+    wgPublicList: "public-apa",
+
+    // URI of the patent status for this WG, for Rec-track documents
+    // !!!! IMPORTANT !!!!
+    // This is important for Rec-track documents, do not copy a patent URI from a random
+    // document unless you know what you're doing. If in doubt ask your friendly neighbourhood
+    // Team Contact.
+    wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/83907/status",
+
+    // Depth for the TOC
+    //maxTocLevel: 2,
+
+    // include introduction sections in the ToC
+    tocIntroductory: true,
+    doRDFa: 1.1
+};
